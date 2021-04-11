4b5405a3285d33c63815e11edc22d8956e8723f5
“GeorgiDS9git config --global user.name GeorgiDS9git config --global user.email georgianachiriacweb@gmail.comgit config --global user.email georgianachiriacweb@github.com <georgianachiriacweb@gmail.com>
2021-04-10 20:12:57 -0400

    create log in/ sign up logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-let isLoggedIn = false;
-
-console.log(currentTime);
+let userIsRegistered = false;
 
 function App() {
-  return <div className="container">{
-  /* { isLoggedIn === true? <h1>Hello</h1> : <Login />} */
-    currentTime > 12 ? <h1>Why are you still working?</h1> : null
-
-  }
+  return (
+    <div className="container">
+      <Form 
+        isRegistered={userIsRegistered}
+      />
     </div>
+  );
 }
 
 export default App;
